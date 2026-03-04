830041b9eaf20233264dd0c72b1126b1cc226792
jimmy <ljr13075241003@outlook.com>
2026-03-04 12:59:59 +0800

    Add MIT license remove unused js and configure marketplace icon

--- test-math.js
+++
@@ -1,7 +0,0 @@
-const MarkdownIt = require('markdown-it');
-const katexPlugin = require('@iktakahiro/markdown-it-katex');
-const md = new MarkdownIt({ html: true }).use(katexPlugin);
-console.log('--- aligned ---');
-console.log(md.render('$$\n\\begin{aligned}\nf(x) &= ax^2 + bx + c \\\\\nf\'(x) &= 2ax + b \\\\\nf\'\'(x) &= 2a\n\\end{aligned}\n$$'));
-console.log('--- raw align ---');
-console.log(md.render('\\begin{align}\nf(x) &= 1 \\\\\n\\end{align}'));
